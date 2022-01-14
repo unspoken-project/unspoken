f14dcc531be42e81b785ef6b26138f585dc1a8e6
Ian Christiansen <ianc.songdog@gmail.com>
2022-01-14 12:46:00 -0800

    post functions written

---
+++ src/services/posts.js
@@ -0,0 +1,28 @@
+import { client, checkError } from './client';
+
+export async function getPosts() {
+  const resp = await client.from('unspoken').select('*');
+  return checkError(resp);
+}
+
+export async function getPostById(id) {
+  const resp = await client.from('unspoken').select('*').match({ id }).single();
+  return checkError(resp);
+}
+
+export async function createPost(post) {
+  const resp = await client
+    .from('unspoken')
+    .insert([{ post: post, user_id: client.auth.user().id }]);
+  return checkError(resp);
+}
+
+export async function editPost(post) {
+  const resp = await client.from('unspoken').update(post).eq('id', post.id);
+  return checkError(resp);
+}
+
+export async function deletePost(id) {
+  const resp = await client.from('unspoken').delete().match({ id });
+  return checkError(resp);
+}
