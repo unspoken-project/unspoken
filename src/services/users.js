c69c81c0b958f8cf9f126dc9fa65704e38e3c981
Ian Christiansen <ianc.songdog@gmail.com>
2022-01-14 12:50:38 -0800

    user functions written

---
+++ src/services/users.js
@@ -0,0 +1,26 @@
+import { checkError, client } from './client';
+
+export function getUser() {
+  return client.auth.session();
+}
+
+export async function signUpUser(username, email, password) {
+  const { user, error } = await client.auth.signUp({ username, email, password });
+  if (error) {
+    throw error;
+  }
+  return user;
+}
+
+export async function signInUser(username, password) {
+  const { user, error } = await client.auth.signIn({ username, password });
+  if (error) {
+    throw error;
+  }
+  return user;
+}
+
+export async function signOut() {
+  const resp = await client.auth.signOut();
+  return checkError(resp);
+}
