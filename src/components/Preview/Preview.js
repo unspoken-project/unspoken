d176587049c26f3e6890b64f04abee5b540ecfc7
Hayden Indiana Holdsworth <IndianaHoldsworth@gmail.com>
2022-01-18 12:36:06 -0700

    Adds UI

--- src/components/Preview/Preview.js
+++ src/components/Preview/Preview.js
@@ -4,8 +4,8 @@ export default function Preview({ post_title, post_content, id }) {
   return (
     <div className="preview-card">
       <div key={id}>
-        <p>{post_title}</p>
-        <p>{post_content}</p>
+        <p>Title: {post_title}</p>
+        <p>Content: {post_content}</p>
       </div>
     </div>
   );
