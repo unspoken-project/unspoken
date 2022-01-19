618ade5270b5a941c4d2d53fbef6601119934468
mira-kine <mirak0419@gmail.com>
2022-01-19 11:47:27 -0800

    working on avatars

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,8 +1,9 @@
 import React from 'react';
 import { NavLink, useHistory } from 'react-router-dom';
+import { uploadAvatar } from '../../services/avatars';
 import './Header.css';
 
-export default function Header() {
+export default function Header({ currentUser }) {
   const history = useHistory();
 
   const handleSubmit = async (e) => {
@@ -10,19 +11,31 @@ export default function Header() {
     history.push(`/create`);
   };
 
+  const uploadFile = (e) => {
+    uploadAvatar(currentUser.id, e.target.files[0]);
+  };
 
   return (
     <div>
       <header>
-        <button className="create-button" onClick={handleSubmit}>
-          Create Post
-        </button>
+        <div className="user-info">
+          <h2>Hello {currentUser.username}</h2>
+          {currentUser.avatar && <img className="avatar" src={currentUser.avatar} />}
+          <input type="file" onChange={uploadFile} className="button" />
+        </div>
         <div className="header-links">
+          {currentUser && (
+            <button className="create-button" onClick={handleSubmit}>
+              Create Post
+            </button>
+          )}
           <div>
-            <NavLink className="home-link" to="/posts">
-              {' '}
-              Home{' '}
-            </NavLink>
+            {currentUser && (
+              <NavLink className="home-link" to="/posts">
+                {' '}
+                Home{' '}
+              </NavLink>
+            )}
             <NavLink className="login-link" to="/">
               {' '}
               Login/Sign-Up{' '}
