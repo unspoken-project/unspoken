c47ce47b5227dda7569ba1b6c23e8bb0d3a59a9b
mira-kine <mirak0419@gmail.com>
2022-01-20 13:02:21 -0800

    moved header comment, working on avatar rendering

--- src/App.js
+++ src/App.js
@@ -7,28 +7,32 @@ import Posts from './views/Posts/Posts';
 import Edit from './views/Edit/Edit';
 import Create from './views/Create/Create';
 import Post from './views/Post/Post';
-import Header from './components/Header/Header';
 import AudioPlayer from './components/Audio/AudioPlayer';
 import Title from './views/Title/Title';
 
 function App() {
   const [currentUser, setCurrentUser] = useState(null);
+  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
     const fetchUser = async () => {
       const user = await getUser();
       setCurrentUser(user);
+      setLoading(false);
     };
     fetchUser();
   }, []);
 
+  if (loading) {
+    return <h1>loading</h1>;
+  }
+
   return (
     <>
       <BrowserRouter>
-        <Header currentUser={currentUser} setCurrentUser={setCurrentUser} />
         <Switch>
           <Route exact path="/">
-            {currentUser && <Posts setCurrentUser={setCurrentUser} />}
+            {currentUser && <Posts setCurrentUser={setCurrentUser} currentUser={currentUser} />}
             {!currentUser && <Title setCurrentUser={setCurrentUser} />}
           </Route>
           <ProtectedRoute currentUser={currentUser} exact path="/posts">
