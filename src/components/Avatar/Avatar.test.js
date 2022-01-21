661a785ff642dc50a4f1abd95999dc7f43631ac6
mira-kine <mirak0419@gmail.com>
2022-01-20 17:47:00 -0800

    cleaning up tests

--- src/components/Avatar/Avatar.test.js
+++ src/components/Avatar/Avatar.test.js
@@ -1,10 +1,11 @@
 import { render, screen } from '@testing-library/react';
 import { MemoryRouter, Route } from 'react-router-dom';
+import Avatar from './Avatar';
 
 test.skip('renders individual post detail', async () => {
   const { container } = render(
     <MemoryRouter initialEntries={['/posts/36']}>
-      <Route path="/posts/:id" component={PostDetail} />
+      <Route path="/posts/:id" component={Avatar} />
     </MemoryRouter>
   );
   await screen.findByDisplayValue('ta overload');
