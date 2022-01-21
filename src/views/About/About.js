2b6ff5385f7c8e70e5d02e7fb9607f3001c07aab
Hayden Indiana Holdsworth <IndianaHoldsworth@gmail.com>
2022-01-20 17:09:50 -0700

    Styles about page, adds links to github/linkedin

--- src/views/About/About.js
+++ src/views/About/About.js
@@ -6,14 +6,55 @@ export default function About() {
     <div className="about-cards">
       <div className="ryssa-card">
         <img src="../../ryssa.jpg" />
-
+        <h3>Ryssa</h3>
         <p>this is words about Karyssa.</p>
+        <a href="https://github.com/karyssa-dandrea" target="_blank" rel="noreferrer">
+          <img className="link-img" src="../../Octocat.png" />
+        </a>
+        <a href="https://www.linkedin.com/in/karyssa-dandrea/" target="_blank" rel="noreferrer">
+          <img className="link-img" src="../../linkedin.png" />
+        </a>
       </div>
-      <div className="ian-card">this is words about Ian.</div>
-      <div className="mira-card">this is words about Mira.</div>
+
+      <div className="ian-card">
+        <img src="../../ian.png" />
+
+        <h3>Ian</h3>
+        <p>this is words about Ian.</p>
+        <a href="https://github.com/ian-christiansen" target="_blank" rel="noreferrer">
+          <img className="link-img" src="../../Octocat.png" />
+        </a>
+        <a href="https://www.linkedin.com/in/ianchristiansen/" target="_blank" rel="noreferrer">
+          <img className="link-img" src="../../linkedin.png" />
+        </a>
+      </div>
+
+      <div className="mira-card">
+        <img src="../../mira.jpeg" />
+        <h3>Mira</h3>
+        <p>this is words about Mira.</p>
+        <a href="https://github.com/mira-kine" target="_blank" rel="noreferrer">
+          <img className="link-img" src="../../Octocat.png" />
+        </a>
+        <a href="https://www.linkedin.com/in/mira-kinebuchi/" target="_blank" rel="noreferrer">
+          <img className="link-img" src="../../linkedin.png" />
+        </a>
+      </div>
+
       <div className="indy-card">
         <img src="../../indy.jpg" />
+        <h3>Indy</h3>
         <p>this is words about Indy.</p>
+        <a href="https://github.com/H-Indiana-Holdsworth" target="_blank" rel="noreferrer">
+          <img className="link-img" src="../../Octocat.png" />
+        </a>
+        <a
+          href="https://www.linkedin.com/in/h-indiana-holdsworth/"
+          target="_blank"
+          rel="noreferrer"
+        >
+          <img className="link-img" src="../../linkedin.png" />
+        </a>
       </div>
     </div>
   );
